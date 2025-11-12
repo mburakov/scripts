92c1aef0c94414fbd2b1f55fb766a4c240dcda24
Mikhail Burakov <mburakov@mailbox.org>
2025-11-12 10:11:07 +0100

    Update chroot script to rely on arch-chroot

--- chroot.sh
+++ chroot.sh
@@ -4,27 +4,25 @@ set -e
 set -x
 
 cleanup() {
-  umount "$ROOT/merged/dev" || true
-  umount "$ROOT/merged/proc" || true
-  umount "$ROOT/merged/sys" || true
   umount "$ROOT/merged/mnt" || true
   umount "$ROOT/merged" || true
   umount "$ROOT/lower" || true
 }
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
+if [ "$#" -lt "1" ]; then
+  echo "Usage: $0 <rootfs.tar.gz> [command [args...]]"
+  exit 1
 fi
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <rootfs.tar.gz>"
-  exit 1
+if [ "$EUID" -ne 0 ]; then
+  exec sudo "$0" "$@"
 fi
 
 DIR=$(dirname "$1")
 ROOTFS=$(basename "$1")
 IMAGE=${ROOTFS%.tar*}.sqfs
+shift
+
 if [ ! -f "$DIR/$IMAGE" ]; then
   echo "Creating SquashFS image..."
   bsdtar -s '|^\./||' -cf - "@$DIR/$ROOTFS" |
@@ -38,15 +36,10 @@ for dir in lower upper work merged; do
   mkdir -p "$ROOT/$dir"
 done
 
-
 mount "$DIR/$IMAGE" "$ROOT/lower"
 mount -t overlay overlay \
   -o lowerdir="$ROOT/lower",upperdir="$ROOT/upper",workdir="$ROOT/work" \
   "$ROOT/merged"
-for dir in proc sys dev; do
-  mount --bind "/$dir" "$ROOT/merged/$dir"
-done
 
-cp /etc/resolv.conf "$ROOT/merged/etc/resolv.conf"
 mount --bind "$PWD" "$ROOT/merged/mnt"
-chroot "$ROOT/merged" /bin/sh -l
+arch-chroot "$ROOT/merged" "$@"
